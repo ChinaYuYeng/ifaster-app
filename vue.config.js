ae915334e350bae4e5bb1c11a11bda71d572db55
yufeng <472917042@qq.com>
2021-01-15 14:39:16 +0800

    add：主题设置，配置文件修改

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
+const path = require("path");
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
 module.exports = {
   chainWebpack: config => {
+    config.resolve.alias.set("@@", resolve("src/business"));
     config.module
       .rule("vue")
       .use("vue-loader")
@@ -9,5 +14,17 @@ module.exports = {
         options.hotReload = true;
         return options;
       });
+  },
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
+          hack: `true; @import "${resolve(
+            "./src/business/style/vant-theme.less"
+          )}"`
+        }
+      }
+    }
   }
 };
