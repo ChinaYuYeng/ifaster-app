b5a1a9c9f4e72b938e35a22562c241b401a86d2f
yufeng <472917042@qq.com>
2021-02-02 11:49:18 +0800

    下拉加载组件，加载逻辑，页面刷新逻辑，路由跳转

--- src/business/components/loadList/index.vue
+++ src/business/components/loadList/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <van-list v-model="loading" :finished="finished" :finished-text="finishedText" @load="onLoad">
+  <van-list v-model="loadStatus.loading" :finished="loadStatus.finished" :finished-text="loadStatus.finishedText" @load="loadStatus.loadList">
     <slot></slot>
   </van-list>
 </template>
@@ -10,53 +10,8 @@ export default {
     loadData: {
       type: Function,
       default: () => Promise.resolve()
-    }
-  },
-  data() {
-    return {
-      finished: false,
-      loading: false,
-      finishedText: "",
-      paging: {
-        pageIndex: 1,
-        pageSize: 10
-      }
-    };
-  },
-  methods: {
-    onLoad() {
-      this.loadData(this.paging)
-        .then(res => {
-          if (this.paging.pageIndex * this.paging.pageSize < res.data.total) {
-            this.paging.pageIndex++;
-            this.finished = false;
-          } else {
-            this.finished = true;
-            this.finishedText = "没有更多了";
-          }
-        })
-        .catch(err => {
-          this.finished = true;
-          console.log(err);
-        })
-        .finally(() => {
-          this.loading = false;
-        });
     },
-    resetLoad() {
-      this.paging = {
-        pageIndex: 1,
-        pageSize: 10
-      };
-      this.finishedText = "";
-      this.onLoad();
-    }
-  },
-  watch: {
-    loadData(newfun, oldfun) {
-      if (newfun === oldfun) return;
-      this.resetLoad();
-    }
+    loadStatus: Object
   }
 };
 </script>
