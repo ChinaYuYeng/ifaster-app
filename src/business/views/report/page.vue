861e75aae7ae1cd97df0a35224c977596c6e0c07
yufeng <472917042@qq.com>
2021-02-04 11:44:55 +0800

    日报月报

--- src/business/views/report/page.vue
+++ src/business/views/report/page.vue
@@ -1,16 +1,16 @@
 <template>
-  <AppLayout ref="report__wrap">
+  <AppLayout ref="report__wrap" @onshow="initChart">
     <template slot="body-top">
       <span class="report__title">统计数据</span>
       <canvas id="report__chart"></canvas>
     </template>
-    <LoadList :loadData="onLoad">
+    <LoadList :loadStatus="loadStatus">
       <van-cell
         is-link
-        style="padding:6px 0;align-items: center;"
-        v-for="item in datalist"
+        center
+        v-for="item in dataList"
         :key="item.date"
-        @click="$router.push({ name: '/report/statement', params: item })"
+        @click="routerTo({ name: '/report/statement', params: { ...item, ...routeData } })"
       >
         <van-grid>
           <van-grid-item>
@@ -18,21 +18,21 @@
           </van-grid-item>
           <van-grid-item>
             <span>营收</span>
-            <span>{{ item.income }}</span>
+            <span>{{ item.payFee }}</span>
           </van-grid-item>
           <van-grid-item>
             <span>分账</span>
-            <span>{{ item.assignment }}</span>
+            <span>{{ item.settle }}</span>
           </van-grid-item>
           <van-grid-item>
             <span>支出</span>
-            <span>{{ item.pay }}</span>
+            <span>{{ item.expend }}</span>
           </van-grid-item>
         </van-grid>
       </van-cell>
     </LoadList>
-    <template #search>
-      <Search :formData="searchForm"></Search>
+    <template #search="scope">
+      <Search :onSearch="onSearch.bind(this, scope)" :searchForm="searchForm"></Search>
     </template>
   </AppLayout>
 </template>
@@ -40,114 +40,67 @@
 <script>
 import F2 from "@antv/f2";
 import Search from "./components/search";
+import loadList from "@@/mixins/loadList";
 export default {
   components: { Search },
+  mixins: [loadList],
   data() {
     return {
-      loading: false,
-      finished: false,
-      searchType: 1,
+      routeData: this.$route.params,
       searchForm: {
-        type: 1,
-        date: new Date().toUTCString()
-      },
-      datalist: [
-        {
-          date: "2021-01-16",
-          income: "123",
-          pay: "123",
-          assignment: "1212"
-        },
-        {
-          date: "2021-01-17",
-          income: "123",
-          pay: "123",
-          assignment: "1212"
-        },
-        {
-          date: "2021-01-18",
-          income: "123",
-          pay: "123",
-          assignment: "1212"
-        },
-        {
-          date: "2021-01-19",
-          income: "123",
-          pay: "123",
-          assignment: "1212"
-        }
-      ]
+        earningType: 1,
+        date: "",
+        orderType: this.$route.params.orderType
+      }
     };
+  },
+  created() {
+    this.setListLoader(paging => {
+      return this.$apis.getList({ ...this.searchForm, ...paging });
+    });
   },
   mounted() {
     this.initChart();
   },
-  provide() {
-    return {
-      parent: this
-    };
-  },
-  methods: {
-    onSearch() {},
-    onLoad() {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          this.datalist.push({
-            date: new Date().toDateString(),
-            income: "123",
-            pay: "123",
-            assignment: "1212"
-          });
-          resolve();
-        }, 1000);
-      });
-    },
-    onRefresh() {},
-    initChart() {
-      let width = this.$refs.report__wrap.$el.getBoundingClientRect().width - 20;
-      const data = [
-        { genre: "1", sold: 275 },
-        { genre: "2", sold: 115 },
-        { genre: "3", sold: 120 },
-        { genre: "4", sold: 350 },
-        { genre: "5", sold: 350 },
-        { genre: "6", sold: 350 },
-        { genre: "7", sold: 350 },
-        { genre: "8", sold: 350 },
-        { genre: "9", sold: 350 },
-        { genre: "10", sold: 350 },
-        { genre: "11", sold: 350 },
-        { genre: "12", sold: 150 }
-      ];
-
-      const chart = new F2.Chart({
-        id: "report__chart",
-        pixelRatio: window.devicePixelRatio, // 指定分辨率
-        width,
-        height: 200
-      });
-      chart.legend(false);
-
-      chart.source(data);
-
-      chart
-        .interval()
-        .position("genre*sold")
-        .color("genre", ["#55BABB"]);
-
-      chart.render();
+  computed: {
+    chartData() {
+      return this.dataList.map((v, index) => ({ genre: index, sold: v.realIncome }));
     }
   },
+  methods: {
+    onSearch({ close }) {
+      return this.setListLoader().then(close);
+    },
+    onRefresh() {
+      return this.setListLoader();
+    },
+    initChart() {
+      this.$nextTick(() => {
+        let width = this.$refs.report__wrap.$el.getBoundingClientRect().width - 20;
+        const chart = (this.chart = new F2.Chart({
+          id: "report__chart",
+          pixelRatio: window.devicePixelRatio, // 指定分辨率
+          width,
+          height: 200
+        }));
+        chart.legend(false);
+
+        chart.source(this.chartData || []);
+
+        chart
+          .interval()
+          .position("genre*sold")
+          .color("genre", ["#55BABB"]);
+
+        chart.render();
+      });
+    },
+    getCahrtData() {}
+  },
   watch: {
-    "searchForm.type": function(val) {
-      if (val == 1) {
-        this.searchForm = {
-          type: 1
-        };
-      } else {
-        this.searchForm = {
-          type: 2
-        };
+    dataList(val, old) {
+      if (val.length !== old.length) {
+        this.chart.changeData(this.chartData);
       }
     }
   }
