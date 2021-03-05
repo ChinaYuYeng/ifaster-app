b4b3562fa70dddb4d23f3e2230425840ce5023ef
yufeng <472917042@qq.com>
2021-03-05 09:41:48 +0800

    v1.2.0

--- src/router/index.js
+++ src/router/index.js
@@ -20,28 +20,61 @@ const router = new VueRouter({
 export default router;
 
 const routeMap = {}; //用于建立完整路由的路由池
+const copyRoute = {}; //待复用路由的缓存
 
 function registerRoute(name, route) {
   routeMap[name] = route;
 }
 
-// function parseRoutes(menu) {
-//   if (!menu) return;
-//   return menu.map(m => ({
-//     ...routeMap[m.url],
-//     // path:m.path,
-//     // meta:m,
-//     children: m.submenu ? parseRoutes(m.submenu) : routeMap[m.url].children || []
-//   }));
-// }
+function queueCopyRoute(name, route) {
+  copyRoute[name] = route;
+  // 微任务会延后到loadRouters方法后面导致复用的路由无法正常装载
+  // Vue.nextTick((key = name) => {
+  //   const from = routeMap[key];
+  //   const to = copyRoute[key];
+  //   for (let p in from) {
+  //     if (!hasOwnProperty.call(to, p)) {
+  //       to[p] = from[p];
+  //     }
+  //   }
+  //   to.component._pagePaths.push(to.name);
+  //   console.log(to.component._pagePaths, "copy");
+  // });
+}
 
-// function loadRouters(menuList) {
-//   let temp = parseRoutes(menuList);
-//   console.log(temp);
-//   router.addRoutes(temp); // 追加路由，错误页面必须最后装入
-// }
+function _doCopy() {
+  Object.keys(copyRoute).forEach(key => {
+    const _from = routeMap[key];
+    const _to = copyRoute[key];
+    if (!_from.meta.export) {
+      throw new Error(`the router named ${_from.name} can't be import , because it do't have "export" sign in meta`);
+    }
+    function copyObj(from, to) {
+      to.name = from.name.replace(new RegExp(`^\\${_from.name}`), _to.name);
+      for (let p in from) {
+        if (p === "children") {
+          to[p] = [];
+          for (let k in from[p]) {
+            to[p][k] = {};
+            copyObj(from[p][k], to[p][k]);
+          }
+          continue;
+        }
+        if (!hasOwnProperty.call(to, p)) {
+          to[p] = from[p];
+        }
+        if (p === "component") {
+          to.component._pagePaths.push(to.name);
+        }
+      }
+    }
+    copyObj(_from, _to);
+  });
+}
 
 function loadRouters(matchMenuToRoute) {
+  _doCopy();
+  console.log(routeMap);
   let temp =
     (matchMenuToRoute && matchMenuToRoute.call(this, routeMap)) ||
     Object.entries(routeMap)
@@ -50,4 +83,4 @@ function loadRouters(matchMenuToRoute) {
   router.addRoutes(temp); // 追加路由，错误页面必须最后装入
 }
 
-export { registerRoute, loadRouters };
+export { registerRoute, loadRouters, queueCopyRoute };
